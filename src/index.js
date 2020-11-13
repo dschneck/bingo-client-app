5c4bc8e744456ba708e22fad6aa15806f54c350b
David Schneck <david.schneckster@hotmail.com>
2020-11-12 21:05:35 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './Board.css';
+import Board from './Board';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Board />,
   document.getElementById('root')
 );
 
